6953bd9c0973562b173c286fa8de2ef6738eedcd
Benjohn Barnes <benjohn@fysh.org>
2023-06-07 05:47:31 +0100

    Comment on `StubURLRequestServer`

--- Sources/Service/URLRequestServer/StubURLRequestServer.swift
+++ Sources/Service/URLRequestServer/StubURLRequestServer.swift
@@ -1,7 +1,14 @@
 import Foundation
 
-/// ``StubURLRequestServer`` implements ``URLServer`` using a dictionary of stub responses. This lets us
-/// instantiate a module with a service factory reading from stub files.
+/// ``StubURLRequestServer`` implements ``URLServer`` using a source of stub responses.
+///
+/// Use ``StubURLRequestServer`` to test feature module behaviour either in an XCTest, or in an app
+/// running against predefined scenarios.
+///
+/// TODO – think this needs a bunch of expansion by providing a standard mechanism for how a set of stubs
+/// would be defined. This might be flexible so that a cross platform common scenario folder
+/// can be used, although ``URLRequestServer`` is such a simple interface that customisation can easily
+/// be achieved by just implementing one.
 ///
 public final class StubURLRequestServer: URLRequestServer {
 
