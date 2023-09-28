8115801886165cfa8cc49b2232bc2709668edf9f
Benjohn Barnes <benjohn@fysh.org>
2023-09-28 16:33:34 +0100

    Add `StubServiceContext`

---
+++ Sources/ServiceDemo/ServiceProviding/StubServiceContext.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public final class StubServiceContext: ServiceContext {
+
+    public var baseURL: URL
+    var stubResults: [URL: URLResult]
+
+    /// A more useful implementation could take a folder in which to look for response stubs.
+    public init(baseURL: URL, stubResults: [URL : URLResult]) {
+        self.baseURL = baseURL
+        self.stubResults = stubResults
+    }
+
+    public func perform(_ request: URLRequest) async -> URLResult {
+        guard let url = request.url else {
+            return .failure(RequestHasNoURL(request: request))
+        }
+
+        guard let result = stubResults[url] else {
+            return .failure(RequestHasNoStub(request: request))
+        }
+
+        return result
+    }
+
+    public struct RequestHasNoURL: Error { let request: URLRequest }
+    public struct RequestHasNoStub: Error { let request: URLRequest }
+}
